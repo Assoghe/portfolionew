aa74be589a3afb5925fb3d1386048622f1e90425
Assoghe <soraya.gherras17@gmail.com>
2023-10-09 17:31:46 +0200

    Portfo - Hero > HeroSection > Color button

--- src/pages/Portfolio/HeroSection.tsx
+++ src/pages/Portfolio/HeroSection.tsx
@@ -10,7 +10,7 @@ function HeroSection() {
 
     <div className="bloc-paragraph">
         <p> Entre design et développement, je donne vie à des interfaces réalistes. Chaque projet est une occasion pour moi d'apprendre, d'innover et de créer des expériences uniques pour les utilisateurs.</p>
-        <a href="/about"> <button> En savoir plus </button></a>
+        <a href="/about"> En savoir plus </a>
     </div>
     </HeroStyled>
   )
@@ -18,12 +18,11 @@ function HeroSection() {
 
 const HeroStyled = styled.div`
     max-width: 100%;
-    height: 100vh;
-    background: red;
+    height: 90vh;
     font-family: 'Spartan';
     align-items: center;
     display: flex;
-    padding: 0 5%;
+    padding: 0 10%;
     justify-content: space-between;
 
 
@@ -32,8 +31,29 @@ const HeroStyled = styled.div`
         width: 45%;
     }
 
+    .bloc-title h1{
+      font-weight: normal;
+      font-size: 2em;
+      line-height: 1.5;
+    }
+
+    .bloc-title h4{
+      margin-bottom: 0.5em;
+    }
+
     .bloc-paragraph {
         width: 45%;
+    }
+
+    .bloc-paragraph p  {
+      line-height: 1.5;
+      font-weight: 500;
+      font-family: 'Mulish';
+      margin-bottom: 2em;
+    }
+
+    .bloc-paragraph a {
+      color: #3C67FD;
     }
 `
 
