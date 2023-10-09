7a4abc63d364ceceb578f1b7f224179fb16977a3
Assoghe <soraya.gherras17@gmail.com>
2023-10-09 17:39:33 +0200

    PortfolioSection > Add PortfolioSection.tsx > Add layout bloc portfolio without style

---
+++ src/pages/Portfolio/PortfolioSection.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function PortfolioSection() {
+  return (
+    <div className='portfolio-section'>
+        <div className="portfoliocard">
+            <img src=''/>
+            <div className="bloc-text">
+                <h5>Application mobile - UX - UI</h5>
+                <h3>Nom de la carte</h3>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam modi totam necessitatibus eveniet omnis, deleniti aliquid laboriosam, quisquam, facere at quam earum in neque iusto officia incidunt natus consequatur libero!</p>
+                <a href="">Voir étude de cas</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default PortfolioSection
