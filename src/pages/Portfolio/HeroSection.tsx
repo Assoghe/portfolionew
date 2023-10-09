b6d31b2a5f6b8fc604dd8c32aa040bb0da41d0e4
Assoghe <soraya.gherras17@gmail.com>
2023-10-09 11:52:31 +0200

    Portfo-Hero > HeroSection > Layout (affine)

--- src/pages/Portfolio/HeroSection.tsx
+++ src/pages/Portfolio/HeroSection.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import { styled } from 'styled-components'
 
 function HeroSection() {
   return (
-    <>
+    <HeroStyled>
     <div className='bloc-title'>
       <h4>Je suis </h4>
       <h1> UX UI Designer <br/> Integrateur web</h1>
@@ -12,8 +12,29 @@ function HeroSection() {
         <p> Entre design et développement, je donne vie à des interfaces réalistes. Chaque projet est une occasion pour moi d'apprendre, d'innover et de créer des expériences uniques pour les utilisateurs.</p>
         <a href="/about"> <button> En savoir plus </button></a>
     </div>
-    </>
+    </HeroStyled>
   )
 }
 
-export default HeroSection
+const HeroStyled = styled.div`
+    max-width: 100%;
+    height: 100vh;
+    background: red;
+    font-family: 'Spartan';
+    align-items: center;
+    display: flex;
+    padding: 0 5%;
+    justify-content: space-between;
+
+
+
+    .bloc-title {
+        width: 45%;
+    }
+
+    .bloc-paragraph {
+        width: 45%;
+    }
+`
+
+export default HeroSection;
