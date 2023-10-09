87780e2807940c2f7b80e122ff80db79fd48b3a3
Assoghe <soraya.gherras17@gmail.com>
2023-10-09 11:39:48 +0200

    Portfolio-Hero > HeroSection > Layout

--- src/pages/Portfolio/HeroSection.tsx
+++ src/pages/Portfolio/HeroSection.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 function HeroSection() {
   return (
-    <div>
-      Hero section
+    <>
+    <div className='bloc-title'>
+      <h4>Je suis </h4>
+      <h1> UX UI Designer <br/> Integrateur web</h1>
     </div>
+
+    <div className="bloc-paragraph">
+        <p> Entre design et développement, je donne vie à des interfaces réalistes. Chaque projet est une occasion pour moi d'apprendre, d'innover et de créer des expériences uniques pour les utilisateurs.</p>
+        <a href="/about"> <button> En savoir plus </button></a>
+    </div>
+    </>
   )
 }
 
