a10d935083b95838956c722563fe8c99bdb3ddd9
Assoghe <soraya.gherras17@gmail.com>
2023-10-10 12:08:21 +0200

    AboutSection > Add AboutSection.tsx

---
+++ src/pages/Portfolio/AboutSection.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function AboutSection() {
+  return (
+    <div>
+      <h2> A propos </h2>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum amet vel beatae, minus ea veniam molestias odit laborum facere voluptas ipsa non, delectus est cum repudiandae nostrum, laudantium architecto omnis!
+      Consectetur a dicta molestias quibusdam placeat cum veniam odio eaque. Distinctio, id! Mollitia iusto delectus eligendi quas debitis ut atque aperiam facilis rerum, architecto aut dicta? Assumenda, similique blanditiis? Reprehenderit!
+      Corrupti vel distinctio, explicabo quam quia a accusantium. Sapiente, ex nam quidem, nostrum veniam deserunt eum architecto at enim cupiditate quasi rerum totam eveniet, fuga sunt quo adipisci ipsam magni.
+      Repellat odit provident quisquam voluptatum fugit ducimus quidem dolorum, animi veniam sint rem tenetur quas soluta dolor nam magni ipsum recusandae ex, natus minus quos minima facere eum obcaecati. Quisquam.</p>
+    </div>
+  )
+}
+
+
+
+export default AboutSection
